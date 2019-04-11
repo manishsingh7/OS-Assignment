ddc53153bdae80a877e8d6e3c0f91d498968180a
manish singh <38642903+manishsingh7@users.noreply.github.com>
2019-04-11 10:31:19 +0530

    Update manish.c

--- manish.c
+++ manish.c
@@ -9,7 +9,7 @@
   FILE *fp = fopen("cpu_burst.txt", "r");
   int bt[20],p[20],wt[20],tat[20],i=0,j,n=5,total=0,pos,temp;
   float avg_wt,avg_tat;
-  printf("\nReading CPU_BURST.txt File\n");
+  printf("\nReading  CPU_BURST.txt  File\n");
   
    while((getc(fp))!=EOF)
      {
